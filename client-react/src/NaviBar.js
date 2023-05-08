fb382ea4376e7fe714e12fe31d334d74fd87aaa8
Ethernets <ethernetss@gmail.com>
2023-05-08 23:18:30 +0200

    upgrade_navbar

--- client-react/src/NaviBar.js
+++ client-react/src/NaviBar.js
@@ -1,11 +1,12 @@
 import React from "react";
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import {Container, Nav, Navbar, Badge, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logos from './logo.svg'
+import bucket from './bucket.svg'
 
 function Navibar() {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="primary" variant="dark">
         <Container>
           <Navbar.Brand href="#home"><img
               alt=""
@@ -16,9 +17,22 @@ function Navibar() {
             />{' '}
             Gallo</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">Pizza</Nav.Link>
+            <Nav.Link href="#features">Beverages</Nav.Link>
+            <Nav.Link href="#pricing">Actions</Nav.Link>
+          </Nav>
+          <Nav>
+            <Button>
+                <img
+                alt=""
+                src={bucket}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                 />{' '}
+                <Badge bg="danger">9</Badge>
+                <span className="visually-hidden">unread messages</span>
+            </Button>
           </Nav>
         </Container>
       </Navbar> 
