1d273d2b9e69beac596ef974b4fd38b7a812fda6
Ethernets <ethernetss@gmail.com>
2023-05-09 01:29:15 +0200

    add-gridMenu

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,27 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Navibar from './NaviBar.js';
+import Gridmenu from './Gridmenu.js';
 
 function App() {
-  const [data ,setData] = useState([])
-
-  useEffect(() => {
-    fetch("/products")
-    .then(res => res.json())
-    .then(data => {
-        setData(data.result_pizza);
-        console.log(data);
-      });
-  }, []);
-
   return(
   <>
  <Navibar />
-  <div className="App">
-      {data.map((item, index) => (
-        <div key={index}>{item[1]} - {item[2]} <h1>Стоимость: {item[3]}</h1></div>
-      ))}
-
-  </div>
+ <div style={{ paddingTop: '20px' }}> {/*padding 20px from Navbar */}
+ <Gridmenu />
+ </div>
  </> 
   );
 }
