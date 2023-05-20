c021713f6c3db41a69d55cf424fad02864b21808
Ethernets <ethernetss@gmail.com>
2023-05-20 22:40:44 +0200

    add emptyCart page

---
+++ client-react/src/pages/EmptyCart.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import imgCart from "../empty_cart_image.png";
+import { Link } from 'react-router-dom';
+
+function EmptyCart() {
+  return (
+    <Container>
+      <Row>
+        <Col md={12} className="text-center">
+          <h1>Your cart is empty</h1>
+          <img src={imgCart} alt="Корзина пуста" className="empty-cart-image" />
+          <p className="empty-cart-message" > <br /> 
+            <Link to="/food" className="empty-cart-link">Add products to your cart</Link>
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default EmptyCart;
