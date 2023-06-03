59432e4022dfbdcacda0137b76b1307fda2a0437
Fugok <alenvincans1@gmail.com>
2023-06-03 21:15:17 +0300

    product page changes

--- client-react/src/pages/Productpage.jsx
+++ client-react/src/pages/Productpage.jsx
@@ -24,44 +24,76 @@ function Productpage() {
   }, []);
 
   return (
-    <div style={{ paddingTop: "5px", margin: "20px" }}>
+    <div style={{ paddingTop: "5px" }}>
       <CarouselMain />
-      <Outlet />
-      <Row xs={1} sm={3} className="justify-content-md-center g-4">
-        {productData.map((product) => (
-          <Col key={product.id}>
-            <Card>
-              <Card.Img variant="top" src={imgPizza} />
-              <Card.Body>
-                <Link key={product.id} to={`/product/${product.id}`}>
-                  {product.name}
-                </Link>
-                <Card.Text>{product.ingredients}</Card.Text>
-                <Row>
-                  <Col className="text-end">Price: {product.price}$</Col>
-                </Row>
-                <Row className="text-end">
-                  <Button
-                    variant="success"
-                    onClick={() =>
-                      addToBacked({
-                        id: product.id,
-                        name: product.name,
-                        price: product.price,
-                      })
-                    }
-                  >
-                    Buy
-                  </Button>
-                </Row>
-              </Card.Body>
-              <Card.Footer className="text-end">
-                <small className="text-muted">{product.food_drink}</small>
-              </Card.Footer>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <div style={{ marginTop: "5px", margin: "300px"  }}>
+        <Outlet />
+        <Row xs={1} sm={3} className="justify-content-md-center g-4">
+          {productData.map((product) => (
+            <Col key={product.id} style={{ marginBottom: "10px" }}> {/* Updated marginBottom value */}
+              <Card>
+                <Card.Img variant="top" src={imgPizza} />
+                <Card.Body>
+                  <Link key={product.id} to={`/product/${product.id}`}>
+                    {product.name}
+                  </Link>
+                  <Card.Text>{product.ingredients}</Card.Text>
+                  <Row>
+                    <Col className="text-end">Price: {product.price}$</Col>
+                  </Row>
+                  <Row className="text-end">
+                    <Button
+                      variant="success"
+                      onClick={() =>
+                        addToBacked({
+                          id: product.id,
+                          name: product.name,
+                          price: product.price,
+                        })
+                      }
+                    >
+                      Buy
+                    </Button>
+                  </Row>
+                </Card.Body>
+                <Card.Footer className="text-end">
+                  <small className="text-muted">{product.food_drink}</small>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+      <div
+        style={{
+          fontSize: "20px",
+          textAlign: "left",
+          marginLeft: "300px",
+          marginRight: "300px",
+          marginBottom: "1px"  // Updated marginBottom value
+        }}
+      >
+        <p>
+          Pica ir viens no populārākajiem un iecienītākajiem ēdieniem visā
+          pasaulē. Varbūt nav neviena cilvēka, kurš to vismaz reizi mūžā nebūtu
+          izmēģinājis. Tas ir gan bērnu, gan pieaugušo cienasts. Tas ir ļoti
+          sātīgs un garšīgs ēdiens, tāpēc visbiežāk to pērk svētku galdā vai
+          parastās sanāksmēs ar draugiem. Populārs ēdiens tiek pagatavots ar
+          daudzām dažādām sastāvdaļām atraimē gaumei. Patiesībā tāpēc pica ir tik
+          populāra. Katrā planētas stūrī tas tiek izveidots pēc īpašas receptes,
+          atšķirībā no pārējās.
+          <br /> <br />
+          Mūsu ēdienkartē ir vairāk nekā 20 picu iespējas. Starp klasiskajiem
+          "Margarita" ir vispopulārākā. Pildījums, kas pārklāts ar mocarellas
+          sieru, kas ir izkusis, un svaigiem tomātiem.
+          <br /> <br />
+          <strong style={{ fontWeight: "bold" }}>
+            Nāciet uz mūsu picēriju Picas ielā 21
+          </strong>
+          .
+          <br /> <br /> <br />
+        </p>
+      </div>
     </div>
   );
 }
