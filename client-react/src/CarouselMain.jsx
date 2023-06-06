3912f4cef65639ebcbd2555b9fe0a9f873530b89
Fugok <alenvincans1@gmail.com>
2023-06-07 02:55:44 +0300

    Design

--- client-react/src/CarouselMain.jsx
+++ client-react/src/CarouselMain.jsx
@@ -1,46 +1,48 @@
 import { Carousel } from "react-bootstrap";
 import React from "react";
-import imgPizza from "./pepperoni-pizza.jpg";
+import Action1 from "./action1.png";
+import Action2 from "./action2.png";
+import Action3 from "./action3.png";
 
 function CarouselMain() {
   return (
     <div style={{ width: "100%", margin: "0 auto" }}>
       <Carousel style={{ width: "100%" }}>
-        <Carousel.Item interval={1000}>
+        <Carousel.Item interval={3000}>
           <img
             className="d-block w-100"
-            src={imgPizza}
+            src={Action1}
             alt="First slide"
             style={{ height: "500px", objectFit: "cover" }}
           />
           <Carousel.Caption>
-            <h3>Actions 1</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Piedāvājums 30€+</h3>
+            <p>Pasūtiet uz 30 eiro un saņemiet bezmaksas piegādi</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        <Carousel.Item interval={3000}>
           <img
             className="d-block w-100"
-            src={imgPizza}
+            src={Action2}
             alt="Second slide"
             style={{ height: "500px", objectFit: "cover" }}
           />
           <Carousel.Caption>
-            <h3>Actions 2</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Piedavajums Dzeriens</h3>
+            <p>Pasūtiet 2 dažādas picas un saņemiet Pepsi 1L bez maksas</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        <Carousel.Item interval={3000}>
           <img
             className="d-block w-100"
-            src={imgPizza}
+            src={Action3}
             alt="Third slide"
             style={{ height: "500px", objectFit: "cover" }}
           />
           <Carousel.Caption>
-            <h3>Actions 3</h3>
+            <h3>Piedāvājums 1+1</h3>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Pasūtiet 2 vienādas picas un saņemiet trešo bez maksas
             </p>
           </Carousel.Caption>
         </Carousel.Item>
